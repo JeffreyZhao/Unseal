d1f1d69aff2a5732188de152b9466643ea41a25a
Jeffrey Zhao <jeffz@live.com>
2012-03-16 01:02:22 +0800

    NuGet related

---
+++ install-packages.cmd
@@ -0,0 +1,3 @@
+tools\NuGet\NuGet.exe install src\Unseal\packages.config -o packages
+tools\NuGet\NuGet.exe install samples\Unmockable\packages.config -o packages
+tools\NuGet\NuGet.exe install samples\Usage\packages.config -o packages
